0ed13eff6bd3aafc06c13113bf52cdf212979da6
Ksunny909 <kvs34@tpu.ru>
2023-06-29 19:29:05 +0700

    2

--- src/js/hello.js
+++ src/js/hello.js
@@ -1,2 +1,12 @@
 console.log("hello")
 console.log("world")
+function showThemes(subject_name) {
+
+	const req1 = new XMLHttpRequest();
+
+	req1.withCredentials = false;
+	req1.open("GET", "https://deathstarapi-glazynovand.b4a.run/api/v1/repair/teams");
+	req1.setRequestHeader("Content-Type", "application/json");
+
+	req1.send(data);
+}
