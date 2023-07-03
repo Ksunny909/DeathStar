9016cb41ba7df7119d7b7d63f01e72437c9d2cf6
Ksunny909 <kvs34@tpu.ru>
2023-07-03 12:37:17 +0700

    sending and receiving data from the server

--- src/js/hello.js
+++ src/js/hello.js
@@ -1,12 +1,40 @@
-console.log("hello")
-console.log("world")
-function showThemes(subject_name) {
+// console.log("hello")
+// console.log("world")
 
-	const req1 = new XMLHttpRequest();
+// fetch('https://deathstarapi-glazynovand.b4a.run/api/v1/energy/state/alpha_cell/battery')
+// 	.then(
+// 		function (response) {
+// 			if (response.status !== 200) {
+// 				console.log('Looks like there was a problem. Status Code: ' +
+// 					response.status);
+// 				return;
+// 			}
 
-	req1.withCredentials = false;
-	req1.open("GET", "https://deathstarapi-glazynovand.b4a.run/api/v1/repair/teams");
-	req1.setRequestHeader("Content-Type", "application/json");
+// 			// Examine the text in the response  
+// 			response.json().then(function (data) {
+// 				console.log(data);
+// 			});
+// 		}
+// 	)
+// 	.catch(function (err) {
+// 		console.log('Fetch Error :-S', err);
+// 	});
 
-	req1.send(data);
-}
+
+// fetch(url, {
+// 	method: 'post',
+// 	headers: {
+// 		"Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+// 	},
+// 	body: 'foo=bar&lorem=ipsum'
+// })
+// 	.then(json)
+// 	.then(function (data) {
+// 		console.log('Request succeeded with JSON response', data);
+// 	})
+// 	.catch(function (error) {
+// 		console.log('Request failed', error);
+// 	});
+fetch("https://deathstarapi-glazynovand.b4a.run/api/v1/repair/stop/alpha_team", { method: "POST", body: "alpha_team" })
+	.then(response => response.text())
+	.then(console.log('отправили'));
