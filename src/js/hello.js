94541b233449350d3f2b4b46d8133bed16b3c962
Ksunny909 <kvs34@tpu.ru>
2023-07-07 06:59:12 +0700

    устранение дублирования кода

--- src/js/hello.js
+++ src/js/hello.js
@@ -1,59 +1,60 @@
 
  //получение списка НЕРАБОЧИХ конденсаторов
 
-fetch('https://deathstarapi-glazynovand.b4a.run/api/v1/energy/state/alpha_cell/battery')
-	.then(
-		function (response) {
-			if (response.status !== 200) {
-				console.log('Looks like there was a problem. Status Code: ' +
-					response.status);
-				return;
-			}
+// fetch('https://deathstarapi-glazynovand.b4a.run/api/v1/energy/state/alpha_cell/battery')
+// 	.then(
+// 		function (response) {
+// 			if (response.status !== 200) {
+// 				console.log('Looks like there was a problem. Status Code: ' +
+// 					response.status);
+// 				return;
+// 			}
 
-			// Examine the text in the response  
-			response.json().then(function (data) {
-				console.log(data)
-				for (let i = 0; i < 64; i ++){
-					if (data.alpha_cell.battery.capacitors[i].is_on == false){
-						console.log(data.alpha_cell.battery.capacitors[i].name)
-					}
-					else break
-				}
-			});
-		}
-	)
-	.catch(function (err) {
-		console.log('Fetch Error :-S', err);
-	}
-);
-fetch('https://deathstarapi-glazynovand.b4a.run/api/v1/energy/state/theta_cell/battery')
-	.then(
-		function (response) {
-			if (response.status !== 200) {
-				console.log('Looks like there was a problem. Status Code: ' +
-					response.status);
-				return;
-			}
+// 			// Examine the text in the response  
+// 			response.json().then(function (data) {
+// 				console.log(data)
+// 				for (let i = 0; i < 64; i ++){
+// 					if (data.alpha_cell.battery.capacitors[i].is_on == false){
+// 						console.log(data.alpha_cell.battery.capacitors[i].name)
+// 					}
+// 					else break
+// 				}
+// 			});
+// 		}
+// 	)
+// 	.catch(function (err) {
+// 		console.log('Fetch Error :-S', err);
+// 	}
+// );
+// fetch('https://deathstarapi-glazynovand.b4a.run/api/v1/energy/state/theta_cell/battery')
+// 	.then(
+// 		function (response) {
+// 			if (response.status !== 200) {
+// 				console.log('Looks like there was a problem. Status Code: ' +
+// 					response.status);
+// 				return;
+// 			}
 
-			// Examine the text in the response  
-			response.json().then(function (data) {
-				console.log(data)
-				for (let i = 0; i < 64; i++) {
-					if (data.theta_cell.battery.capacitors[i].is_on == false) {
-						console.log(data.theta_cell.battery.capacitors[i].name)
-					}
-					else break
-				}
-			});
-		}
-	)
-	.catch(function (err) {
-		console.log('Fetch Error :-S', err);
-	});
+// 			// Examine the text in the response  
+// 			response.json().then(function (data) {
+// 				console.log(data)
+// 				for (let i = 0; i < 64; i++) {
+// 					if (data.theta_cell.battery.capacitors[i].is_on == false) {
+// 						console.log(data.theta_cell.battery.capacitors[i].name)
+// 					}
+// 					else break
+// 				}
+// 			});
+// 		}
+// 	)
+// 	.catch(function (err) {
+// 		console.log('Fetch Error :-S', err);
+// 	});
+
 
 // получение списков СВОБОДНЫХ ремонтных бригад
 
-fetch('https://deathstarapi-glazynovand.b4a.run/api/v1/repair/team/alpha_team')
+fetch('https://deathstarapi-glazynovand.b4a.run/api/v1/repair/teams')
 	.then(
 		function (response) {
 			if (response.status !== 200) {
@@ -62,84 +63,15 @@ fetch('https://deathstarapi-glazynovand.b4a.run/api/v1/repair/team/alpha_team')
 				return;
 			}
 			response.json().then(function (data) {
-				if (data.is_busy == false){
-					console.log(data.name)
-				}		
-			});
-		}
-	)
-	.catch(function (err) {
-		console.log('Fetch Error :-S', err);
-	}
-);
-fetch('https://deathstarapi-glazynovand.b4a.run/api/v1/repair/team/beta_team')
-	.then(
-		function (response) {
-			if (response.status !== 200) {
-				console.log('Looks like there was a problem. Status Code: ' +
-					response.status);
-				return;
-			}
-			response.json().then(function (data) {
-				if (data.is_busy == false) {
-					console.log(data.name)
-				}		
-			});
-		}
-	)
-	.catch(function (err) {
-		console.log('Fetch Error :-S', err);
-	}
-);
-fetch('https://deathstarapi-glazynovand.b4a.run/api/v1/repair/team/gamma_team')
-	.then(
-		function (response) {
-			if (response.status !== 200) {
-				console.log('Looks like there was a problem. Status Code: ' +
-					response.status);
-				return;
-			}
-			response.json().then(function (data) {
-				if (data.is_busy == false) {
-					console.log(data.name)
-				}		
-			});
-		}
-	)
-	.catch(function (err) {
-		console.log('Fetch Error :-S', err);
-	}
-);
-fetch('https://deathstarapi-glazynovand.b4a.run/api/v1/repair/team/delta_team')
-	.then(
-		function (response) {
-			if (response.status !== 200) {
-				console.log('Looks like there was a problem. Status Code: ' +
-					response.status);
-				return;
-			}
-			response.json().then(function (data) {
-				if (data.is_busy == false) {
-					console.log(data.name)
-				}		
-			});
-		}
-	)
-	.catch(function (err) {
-		console.log('Fetch Error :-S', err);
-	}
-);
-fetch('https://deathstarapi-glazynovand.b4a.run/api/v1/repair/team/epsilon_team')
-	.then(
-		function (response) {
-			if (response.status !== 200) {
-				console.log('Looks like there was a problem. Status Code: ' +
-					response.status);
-				return;
-			}
-			response.json().then(function (data) {
-				if (data.is_busy == false) {
-					console.log(data.name)
+				for (var i in data) {
+					var key = i;
+					var val = data[i];
+					for (var j in val) {
+						var sub_key = j;
+						if ((j == "is_busy") && (val.is_busy == false) ){
+							console.log(key);
+						}
+					}
 				}
 			});
 		}
@@ -147,4 +79,4 @@ fetch('https://deathstarapi-glazynovand.b4a.run/api/v1/repair/team/epsilon_team'
 	.catch(function (err) {
 		console.log('Fetch Error :-S', err);
 	}
-);
+	);
